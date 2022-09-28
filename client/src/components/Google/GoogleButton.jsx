01852c71617830e320836997c32249afff21bc0a
unknown <german.af86@gmail.com>
2022-09-28 17:03:44 -0300

    dada

--- client/src/components/Google/GoogleButton.jsx
+++ client/src/components/Google/GoogleButton.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import GoogleButton from 'react-google-button'
+//import GoogleButton from 'react-google-button';
+import GoogleLogin from 'react-google-login';
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
-//const { BACK_URL } = process.env
+//const { GOOGLE_CLIENT_ID } = process.env
 
 export default function GoogleBtn({type}) {
 
-    const hk_URL = "https://henrygames.herokuapp.com"
+    //const hk_URL = "https://henrygames.herokuapp.com"
     
     const BACK_HEROKU = process.env.REACT_APP_API || "http://localhost:3001";
+
+    const responseGoogle = (response) => {
+        console.log(response);
+      }
     
     const google = () => {
         window.location.href = `${BACK_HEROKU}/auth/google`
@@ -17,12 +22,18 @@ export default function GoogleBtn({type}) {
     
     return (
         <fragment>
-            <GoogleButton
+            <GoogleLogin
                 clientId="1068714746325-oo9eg04cqgvfljp14dgjrbgaego7osqp.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+            {/* <GoogleButton
                 type={type}
                 label='Log in with Google'
                 onClick={() => {google()}}
-            />
+            /> */}
         </fragment>
     )
 }
